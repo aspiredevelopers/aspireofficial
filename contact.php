1b2f8c29d689ef2d4de40f5e0efa410dc1f88941
Aspire Developers <65492935+aspiredevelopers@users.noreply.github.com>
2020-06-03 08:30:14 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
 	// Data for text message. This is the text message data.
 	$sender = "ASPIRE"; // This is who the message appears to be from.
 	$numbers = "919995466747, 917025554920, 919526960627"; // A single number or a comma-seperated list of numbers
-	$message = $_POST['message'];
+	$message ="This is test!";
 	// 612 chars or less
 	// A single number or a comma-seperated list of numbers
 	$message = urlencode($message);
