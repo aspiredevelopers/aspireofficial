638474cac9378da8d4639675bcf6f75c7f5adf2d
Aspire Developers <65492935+aspiredevelopers@users.noreply.github.com>
2020-06-03 07:13:09 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,23 @@
 <?php
+	// Authorisation details.
+	$username = "aspiredevelopersann@gmail.com";
+	$hash = "29d2b3917af8edbb433a558473ef0810815afc8f4d933a92553e6bbe89ab0a03";
 
+	// Config variables. Consult http://api.textlocal.in/docs for more info.
+	$test = "0";
 
+	// Data for text message. This is the text message data.
+	$sender = "TXTLCL"; // This is who the message appears to be from.
+	$numbers = "919995466747, 917025554920, 919526960627"; // A single number or a comma-seperated list of numbers
+	$message = "This is a test message from the PHP API script.";
+	// 612 chars or less
+	// A single number or a comma-seperated list of numbers
+	$message = urlencode($message);
+	$data = "username=".$username."&hash=".$hash."&message=".$message."&sender=".$sender."&numbers=".$numbers."&test=".$test;
+	$ch = curl_init('http://api.textlocal.in/send/?');
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch); // This is the result from the API
+	curl_close($ch);
 ?>
